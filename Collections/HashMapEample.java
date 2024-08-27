ef6d9c8d09f59060e949196cb81a35898ac94a5d
Harish-Kushwah <harishkushwah54321@gmail.com>
2024-08-27 23:59:11 +0530

    HashMap added

---
+++ Collections/HashMapEample.java
@@ -0,0 +1,13 @@
+package Collections;
+import java.util.*;
+public class HashMapEample {
+    public static void main(String[] args) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(1,1);
+        map.put(2,21);
+        map.put(3,32);
+        map.put(4,13);
+    }
+
+    
+}
